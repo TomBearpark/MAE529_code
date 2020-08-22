30818324805ece885a0a9fd77c741dd5ec7eaba4
Thomas Bearpark <tombearpark@MacBook-Pro.local>
2020-08-22 11:50:06 +0100

    testing atom git integration

--- optimization_test.jl
+++ optimization_test.jl
@@ -18,11 +18,47 @@ model = Model(Ipopt.Optimizer)
 # Set constraints
 @constraint(model, con, x+y+z<=1)
 
+for
 # Optimise!
 optimize!(model)
 
-num_results(model)
 println("x = ", round.(value(x);digits = 3),
         " y = ", round.(value(y);digits = 3),
         " z = ", round.(value(z);digits = 3)
         )
+
+##########
+using JuMP, Ipopt
+# Initialise the model and an optimiser
+model = Model(Ipopt.Optimizer)
+
+# Set up the variables
+@variable(model, x)
+@variable(model, y)
+
+# Define objective function
+@NLobjective(model, Max, -2 * (x-4)^2 -2 * (y-4)^2 )
+
+# Set constraints
+@constraint(model, con1, x+y<=4)
+@constraint(model, con2, x+3y<=9)
+
+# Optimise!
+optimize!(model)
+
+println("x = ", round.(value(x);digits = 3),
+        " y = ", round.(value(y);digits = 3))
+               )
+
+# Visualise the function and solution
+using Plots; pyplot()
+x=range(-2,stop=2,length=100)
+y=range(-2,stop=2,length=100)
+f(x,y) = -2 * (x-4)^2 -2 * (y-4)^2
+plot(x,y,f,st=:surface)
+
+g(x,y) = x+y-4
+plot!(x,y,g, st =:surface)
+
+h(x,y) = x+3y-9
+plot!(2x,y,h, st =:surface)
