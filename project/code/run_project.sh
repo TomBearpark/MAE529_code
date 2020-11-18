a51818166800c97f8a40a58c4d13380bcb5f4472
TomBearpark <t.bearpark@googlemail.com>
2020-11-18 14:45:03 +0000

    clean up

--- project/code/run_project.sh
+++ project/code/run_project.sh
@@ -1,13 +1,18 @@
+# Loop over the jobs, sending them all out at once, allowing for parallel running 
+
 # Get into the relevant directory
-cd /Users/tombearpark/Documents/princeton/1st_year/MAE529/MAE529_code/project/code
-# Electr capex is first arg. Second arg is H2 efficiency. Loop over the jobs
-# Sending them all out at once 
+dir=/Users/tombearpark/Documents/princeton/1st_year/MAE529/MAE529_code/project/
+cd "${dir}/code"
+# Electr capex is first arg. Second arg is H2 efficiency. 
 for elec_cpx in 200 500 800 
 do
 	for eff in 0.75 0.8 0.85
 	do
 		echo $eff
 		echo $elec_cpx
-		nohup julia draft_code.jl $elec_cpx $eff &
+		nohup julia run_code.jl $elec_cpx $eff $dir &
 	done
 done
+
+
+# nohup julia run_code.jl 200 0.8 $dir &
