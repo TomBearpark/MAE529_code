cefb210fe7fdba570bf18e93cfe356b575699a9c
TomBearpark <t.bearpark@googlemail.com>
2020-11-19 15:21:14 +0000

    code is ready for full run through via bash script

--- project/code/run_project.sh
+++ project/code/run_project.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Loop over the jobs, sending them all out at once, allowing for parallel running 
 
 # Get into the relevant directory
@@ -6,13 +8,39 @@ cd "${dir}/code"
 # Electr capex is first arg. Second arg is H2 efficiency. 
 for elec_cpx in 200 500 800 
 do
-	for eff in 0.75 0.8 0.85
+	for eff in "0.75" "0.80" "0.85"
 	do
-		echo $eff
-		echo $elec_cpx
-		nohup julia run_code.jl $elec_cpx $eff $dir & 
+		# Get appropriate file string identifier. Need these if statements as
+		# cant work out how to do floating point math in bash! Only run code if target 
+		# file doesn't exist
+		if [ $eff = "0.75" ] ; then
+			eff_s="75"
+		elif [ $eff = "0.80" ] ; then
+			eff_s="80"
+		else
+			eff_s="85"
+		fi
+		FILE="${dir}/results/52_weeks/c_tax_0/EleCpx_${elec_cpx}.0_StorCpx_0.6_Eff_${eff_s}/time_results.csv"
+		if test -f "$FILE"; then
+		    echo "$FILE exists."
+		else 
+			echo "running code for $elec_cpx $eff_s"
+			echo "$FILE"
+			# Run code, with appropriate command line arguments
+			nohup julia run_code.jl $elec_cpx $eff $dir & 
+		fi
 	done
 done
 
 
 # julia run_code.jl 200 0.8 $dir
+# elec_cpx=200
+# eff=0.7
+# eff_s=$((${eff}*100))
+# eff_s="${eff_s%.*}"
+# FILE="${dir}/results/52_weeks/c_tax_0/EleCpx_${elec_cpx}.0_StorCpx_0.6_Eff_${eff_s}/time_results.csv"
+# if test -f "$FILE"; then
+#     echo "$FILE exists."
+# else 
+# 	echo "nice"
+# fi
